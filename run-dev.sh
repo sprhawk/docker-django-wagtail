62eaed4da20a1f4d9ffd8e942871d20e4be35107
Yang Hongbo <465558+sprhawk@users.noreply.github.com>
2018-05-27 22:29:23 +0800

    add arguments to run-dev.sh

--- run-dev.sh
+++ run-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # docker build -t iotpi-dev -f Dockerfile .
-docker run -it --rm --mount type=bind,src=$(pwd)/wheel,dst=/usr/local/wheel --mount type=bind,src=$(pwd),dst=/usr/local/app,ro iotpi-dev
+docker run -it --rm --mount type=bind,src=$(pwd)/wheel,dst=/usr/local/wheel --mount type=bind,src=$(pwd)/web,dst=/usr/local/app,ro iotpi-dev $@
