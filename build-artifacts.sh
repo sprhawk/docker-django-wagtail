12a7ef6a06586e3042bc2e3dd9d658e32b1f0cd2
Yang Hongbo <465558+sprhawk@users.noreply.github.com>
2018-05-27 22:16:39 +0800

    fix build

--- build-artifacts.sh
+++ build-artifacts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # docker build -t iotpi-build -f Dockerfile.build .
 mkdir -p wheel
-docker run -it --rm --mount type=bind,src=$(pwd)/wheel,dst=/usr/local/wheel --mount type=bind,src=$(pwd),dst=/usr/local/app,ro iotpi-build
+docker run -it --rm --mount type=bind,src=$(pwd)/wheel,dst=/usr/local/wheel --mount type=bind,src=$(pwd),dst=/usr/local/app,ro iotpi-build $@
